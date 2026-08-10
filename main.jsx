b6f83d37a60e2617dc63e2df54c240408bf76896
t3dwards88 <taeariaedwards@gmail.com>
2026-08-10 18:34:10 -0500

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,7 +1,10 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-body {
-  margin: 0;
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
